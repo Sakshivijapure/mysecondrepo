e5ffea0a564b00518dc1141a7e9bda76768b56fb
Sakshivijapure <vijapuresakshi123@gmail.com>
2024-09-04 14:27:20 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,31 @@
-here is the java file
+package softengineering;
+
+public class arithmaticop {
+	public int num;
+	public int num1;
+	public int num2;
+	public int sum=0;
+	
+
+	public int add(int num1,int num2)
+	{
+		return num1+num2;
+	}
+	public int sub(int num1,int num2)
+	{
+		return num1-num2;
+	}
+	public int square(int num)
+	{
+		return num*num;
+	}
+	public int div(int num1,int num2)
+	{
+		return num1/num2;
+	}
+	public int mul(int num1,int num2)
+	{
+		return num1*num2;
+	}
+
+}
